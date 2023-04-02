684849f6b33aa6e6eb8935986576d90b8e82b7da
Xor010 <khaled.basuni.0@gmail.com>
2023-04-02 20:15:16 +0200

    add def basic file

---
+++ Basic/basic_def.py
@@ -0,0 +1,37 @@
+o  = [1,2,3,4]
+x  = 1
+xx = 2
+'''Basic of def .. (Functions) in python'''
+def def_name(para1 , para2, para3 ):
+    pass
+    pass
+    pass
+    return
+'''
+parameter in python have 4 type
+'''
+# 1 - Require  
+def def_name_1(para1, para2):
+    pass
+    pass
+    return
+# require way --> def_name(para1, para2)
+# 2 - Keyword  : change passion
+def def_name_2(para1, para2):
+    pass
+    pass
+    return
+# Keyword way --> def_name(para2 = value , para1 = value)
+# 3 - default 
+def def_name_3(para1 = 1, para2 = 2 ):
+    pass
+    pass
+    return
+#default way --> in this way if you not add any value parameter will have the default value
+# 4 - parameter value unknown value length
+def def_name_4(para1, *para2):
+    for x in para2:
+        print(x)
+    return
+# def unknown value length call way
+def_name_4(xx , o)
