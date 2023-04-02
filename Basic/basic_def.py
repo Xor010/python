34c234a1992eeba5207bbc77c40dc6dad2641e3d
Xor010 <khaled.basuni.0@gmail.com>
2023-04-02 21:43:19 +0200

    def file

--- Basic/basic_def.py
+++ Basic/basic_def.py
@@ -1,12 +1,16 @@
 o  = [1,2,3,4,5]
 x  = 1
 xx = 2
+print (f' x  {x}')
 '''Basic of def .. (Functions) in python'''
-def def_name(para1 , para2, para3 ):
+def def_name(para1 , para2, para3):
+    global x 
+    x = 100
+    print(x)
     pass
     pass
     pass
-    return
+    return para1 + para2
 '''
 parameter in python have 4 type
 '''
@@ -30,8 +34,18 @@ def def_name_3(para1 = 1, para2 = 2 ):
     return
 #default way --> in this way if you not add any value parameter will have the default value
 
-# 4 - parameter value unknown value length
-def def_name_4_1(para1, *para2):
+# 4.0 - parameter value unknown value length
+def def_name_4_0(para1, *para2): # <-- tow para
     return (sum(para2) + para1) #<-- Keyword way call
 # def unknown value length call way
+print(def_name_4_0(1000, 3, 5, 60))
+# 4.1 - parameter value unknown value length
+def def_name_4_1(*para2): #<-- one para
+    return (sum(para2)) #<-- Keyword way call
+# def unknown value length call way
 print(def_name_4_1(1000, 3, 5, 60))
+
+# global var
+
+print(def_name(100,50,7))
+print (f' x from function {x}')
