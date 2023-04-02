ea87a2d4d23aa84b60cd9a035f288665b8a6c445
Xor010 <khaled.basuni.0@gmail.com>
2023-04-02 22:15:58 +0200

    def commit

--- Basic/basic_def.py
+++ Basic/basic_def.py
@@ -19,21 +19,19 @@ def def_name_1(para1, para2):
     pass
     pass
     return
-# require way --> def_name(para1, para2)
-
+##### require call way --> def_name(para1, para2)
 # 2 - Keyword  : change passion
 def def_name_2(para1, para2):
     pass
     pass
     return
-# Keyword way --> def_name(para2 = value , para1 = value)
+##### Keyword call way --> def_name(para2 = value , para1 = value)
 # 3 - default 
 def def_name_3(para1 = 1, para2 = 2 ):
     pass
     pass
     return
-#default way --> in this way if you not add any value parameter will have the default value
-
+##### default call way --> in this way if you not add any value parameter will have the default value
 # 4.0 - parameter value unknown value length
 def def_name_4_0(para1, *para2): # <-- tow para
     return (sum(para2) + para1) #<-- Keyword way call
@@ -44,8 +42,13 @@ def def_name_4_1(*para2): #<-- one para
     return (sum(para2)) #<-- Keyword way call
 # def unknown value length call way
 print(f'def_name_4_1:  {def_name_4_1(1000, 3, 5, 60)}')
-
 # global var
-
 print(f'def_name:  {def_name(100,50,7)}')
 print (f' x from def_name function:  {x}')
+'''
+anonymous function
+'''
+# definition way   
+func = lambda var1, var2 : print(var1 + var2)
+# call way
+func(1,3)
