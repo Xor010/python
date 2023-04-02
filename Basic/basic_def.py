58934f8bfe804ecf22e88ed87c5b4ccfa08fe5c4
Xor010 <khaled.basuni.0@gmail.com>
2023-04-02 22:01:02 +0200

    commit for basic_def

--- Basic/basic_def.py
+++ Basic/basic_def.py
@@ -1,12 +1,12 @@
 o  = [1,2,3,4,5]
 x  = 1
 xx = 2
-print (f' x  {x}')
+print (f' x from code:  {x}')
 '''Basic of def .. (Functions) in python'''
 def def_name(para1 , para2, para3):
     global x 
     x = 100
-    print(x)
+    print (f' x into def_name function:  {x}')
     pass
     pass
     pass
@@ -38,14 +38,14 @@ def def_name_3(para1 = 1, para2 = 2 ):
 def def_name_4_0(para1, *para2): # <-- tow para
     return (sum(para2) + para1) #<-- Keyword way call
 # def unknown value length call way
-print(def_name_4_0(1000, 3, 5, 60))
+print(f'def_name_4_0:  {def_name_4_0(1000, 3, 5, 60)}')
 # 4.1 - parameter value unknown value length
 def def_name_4_1(*para2): #<-- one para
     return (sum(para2)) #<-- Keyword way call
 # def unknown value length call way
-print(def_name_4_1(1000, 3, 5, 60))
+print(f'def_name_4_1:  {def_name_4_1(1000, 3, 5, 60)}')
 
 # global var
 
-print(def_name(100,50,7))
-print (f' x from function {x}')
+print(f'def_name:  {def_name(100,50,7)}')
+print (f' x from def_name function:  {x}')
