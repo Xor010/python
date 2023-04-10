be2eab10540e8a377183284716fcca83edd534b0
Xor010 <khaled.basuni.0@gmail.com>
2023-04-10 17:46:27 +0200

    modifi class users

--- project/prog.py
+++ project/prog.py
@@ -1,8 +1,8 @@
-class Bank:
-
-    balance = 0
-    name    = ''
+class users():
+    #balance = 0
+    #name    = ''
     age     = 18
+      
     def __init__(self, b, n, a):
         if a > self.age:
             self.balance = b
@@ -11,15 +11,32 @@ class Bank:
             print(f'Welcome {self.name } your balance = { b }')
         else:
             print(f'sorry you must be over than 18y')
+    def profile(self):
+        print(self.name, self.age,self.balance)
+        return  
+class Bank(users):
+
     def add_b(self,b):
         self.balance += b
         return self.balance
-    def profile(self):
-        print(self.name, self.age,self.balance)
+   
+    def withdraw(self , hm):
+        if hm < self.balance:
+            self.balance -= hm
+            print(f'you withdraw {hm} and your credit {self.balance}')
+            return
+        print(f'sorry you don\'t have credit {self.balance}')
         return
+    
+
 b1 = Bank(10, 'ahmed', 20)
 print(b1.add_b(50))
 
 ali = Bank(1000, 'ali', 300)
 print(b1.add_b(54440))
 ali.profile()
+ali.withdraw(100)
+ali.profile()
+ali.withdraw(899)
+ali.profile()
+ali.withdraw(2)
