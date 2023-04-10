38389fe42471e7007b7c41ed0c4f1dd73e526707
Xor010 <khaled.basuni.0@gmail.com>
2023-04-10 23:42:26 +0200

    add while

--- project/games.py
+++ project/games.py
@@ -10,22 +10,31 @@
 
 class G:
     def __init__(self) -> None:
+        
         print('''Welcome into my games if you wanna play game please select between[1,2]
                  1 - First Game
                  2 - Sec Game 
                  3 - Exit''')
         self.Game_n = int(input('Select Games Number :'))
-        if self.Game_n == 1:
-            pae = input('Enter Paragraph:')
-            self.paragraph(pae)
-        elif self.Game_n == 2:
-            i = int(input('num 1:'))
-            io =int(input('num 2:'))
-            self.numr(i, io)
-        else:
-            print('Good Bye')
-            return
-            
+        self.xo ='y'
+        while self.xo == 'y':
+            if self.Game_n == 1:
+                pae = input('Enter Paragraph:')
+                self.paragraph(pae)
+                self.p_agn()
+            elif self.Game_n == 2:
+                i = int(input('num 1:'))
+                io =int(input('num 2:'))
+                self.numr(i, io)
+                self.p_agn()
+            elif self.Game_n == 3:
+                print('Good By')
+                return
+            else:
+                print('Please Enter Number Between 1 : 3')
+                
+    def p_agn(self):
+        self.xo = input('Play Again ?')        
     def paragraph(self, p):
         g = []
         pa = p.split(' ')
