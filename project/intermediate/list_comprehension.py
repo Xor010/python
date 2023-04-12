5fe87e45f9c5c778b0c2803bd88172b3334da195
Xor010 <khaled.basuni.0@gmail.com>
2023-04-12 18:04:44 +0200

    list comprehension

--- project/intermediate/list_comprehension.py
+++ project/intermediate/list_comprehension.py
@@ -6,7 +6,8 @@ print(''.join(l0))
 story2 = ['i','wanna','eat','but','not','Fish']
 l1 = [i if i !='not' else '' for i in story2 ]
 print(' '.join(l1))
-
+l1 = [i for i in story2 if i !='not'  ]
+print(' '.join(l1))
 
 
 yy = [story1[i] for i in range(len(story1))]
@@ -14,5 +15,14 @@ yy.reverse()
 print(''.join(yy))
 
 print(''.join(list(reversed(story1))))
+
+
+def count_list(lst):
+    c = [ len(str(i)) for i in lst ]
+    return c 
+print(count_list([1212,'ds',22222,222222,2222222]))
+
+
+
 
 
--- project/trening.py
+++
@@ -1,6 +0,0 @@
-def count_list(lst):
-    c = []
-    for l in lst:
-        c.append(len(str(l)))
-    return c
-print(count_list([1212,1212,22222,222222,2222222]))
