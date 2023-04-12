bc5f805fc9c3a14f942783898470f78575dbc5ee
Xor010 <khaled.basuni.0@gmail.com>
2023-04-12 17:53:17 +0200

    list comprehension

--- project/intermediate/list_comprehension.py
+++ project/intermediate/list_comprehension.py
@@ -1,31 +1,18 @@
-def xv(names):
-    res = [x for x in names]
-    print(res)
-    return
+story1 = '0987654321'
+l0 = [story1[i] for i in range(len(story1)-1, -1 ,-1)]
+print(''.join(l0))
 
-def xvv(names):
-    res = [x for x in names if len(str(x)) > 3  ]
-    print(res)
-    return
 
-def xvvv(names):
-    res = [x if len(str(x)) > 3 else 0 for x in names ]
-    print(res)
-    return
+story2 = ['i','wanna','eat','but','not','Fish']
+l1 = [i if i !='not' else '' for i in story2 ]
+print(' '.join(l1))
 
-xvvv([90,232,0])
-xvv([1,23,24243])
-xv([1,23,24243])
 
-l = [1,2,3,'ahmed', 'nour']
-d = [v if v == 3 or v == 'nour' else 'none' for v in l]
-print(d)
-l = [1,2,3,'ahmed', 'nour']
-[print(v) if v == 3 or v == 'nour' else 'none' for v in l ]
-#print('do' if not 1 else print('False'))
-t= 50
-print('ok') if t == 5 else print('o')
-t = [x for x in range(5,101,5) ]
-print(t)
-tt = [x if x%2==0  else x for x in range(5,101,5)]
-print(tt)
+
+yy = [story1[i] for i in range(len(story1))]
+yy.reverse()
+print(''.join(yy))
+
+print(''.join(list(reversed(story1))))
+
+
